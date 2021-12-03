200ded63da94164e7cd62461eb2e0d4108a57f4d
Håkan Norberg <hakan.norberg@palettesoftware.com>
2021-12-03 13:47:46 +0100

    Display a list

---
+++ heroes/src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Arvid' },
+  { id: 2, name: 'Bertil' },
+  { id: 3, name: 'Cesar' },
+  { id: 4, name: 'David' },
+  { id: 5, name: 'Erika' },
+  { id: 6, name: 'Fredrika' },
+  { id: 7, name: 'Gun' },
+  { id: 8, name: 'Hesa Fredik' },
+  { id: 9, name: 'Inga' },
+  { id: 10, name: 'Johan' }
+];
